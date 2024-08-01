28d10962659abae7bd68b0f670373145a5c098aa
Arjun Sharma <arjunsharma@Arjuns-MacBook-Pro.local>
2024-08-01 17:01:30 -0400

    Create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,41 +1,21 @@
-import {useEffect, useState} from "react";
-import apiClients from "../services/api-clients.ts";
-import {Spinner} from "@chakra-ui/react";
-
-interface Game {
-  id: number,
-  name: string,
-}
-
-interface GamesResponse {
-  count: number,
-  next: string,
-  previous: string,
-  results: Game[]
-}
-
+import { Spinner } from "@chakra-ui/react";
+import useGames from "../services/useGames.ts";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
-  
-  useEffect(() => {
-    setLoading(true);
-    apiClients.get<GamesResponse>('/games').
-    then(res => setGames(res.data.results)).
-    catch((err) => setError(err.message)).
-    finally(() => setLoading(false));
-  }, []);
+  const { loading, error, games } = useGames();
   return (
     <>
       <div>
-        {loading && <Spinner size={"xl"}/>}
+        {loading && <Spinner size={"xl"} />}
         {error && <p>{error}</p>}
-        {games.map(game => <p key={game.id}>{game.name}</p>)}
+        <ul>
+          {games.map((game) => (
+            <li key={game.id}>{game.name}</li>
+          ))}
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
 export default GameGrid;
