a16052e5befd442e4e27e61276b710e293c1242e
Arjun Sharma <arjunsharma@Arjuns-MacBook-Pro.local>
2024-08-01 14:42:45 -0400

    layout UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import {Button} from "@chakra-ui/react";
+import {Grid, GridItem, Show} from "@chakra-ui/react";
 
 function App() {
   
   return (
     <>
-      <Button colorScheme={"blue"}></Button>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+        lg: `"nav nav" "aside main"`
+      }}>
+        <GridItem area={"nav"} bg={'coral'}>nav</GridItem>
+        <Show above={"md"}>
+          <GridItem area={"aside"} bg={'lavender'}>aside</GridItem>
+        </Show>
+        <GridItem area={"main"} bg={'black'} textColor={"white"}>main</GridItem>
+      </Grid>
     
     </>
   )
