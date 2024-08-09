dfa1a39559dc19171fa989a8b8674bda9069f9e4
arjunsharma <ashar562@uwo.ca>
2024-08-09 12:37:47 -0400

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,23 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+  maxChars?: number;
+  children: string;
+}
+
+function ExpandableText({ children, maxChars = 300 }: Props) {
+  if (children?.length <= maxChars) return <Text>children</Text>;
+  const [showFullText, setShowFullText] = useState(false);
+  let shownText = showFullText ? children : children.slice(0, maxChars) + "...";
+  return (
+    <>
+      <Text>{shownText}</Text>
+      <Button marginLeft={1} size={"xs"} fontWeight={"bold"} colorScheme={"yellow"} onClick={() => setShowFullText(!showFullText)}>
+        {showFullText ? "Show Less" : "Show More"}
+      </Button>
+    </>
+  );
+}
+
+export default ExpandableText;
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,15 +1,16 @@
 import {useParams} from "react-router-dom";
 import useGameDetails from "../hooks/useGameDetails.ts";
-import Header from "../components/Header.tsx";
 import {Heading, Text} from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText.tsx";
 
 function GameDetailsPage() {
   const {slug} = useParams();
   const {data, isLoading, error} = useGameDetails(slug!);
+  
   return (
     <>
       <Heading>{data?.name}</Heading>
-      <Text>{data?.description_raw}</Text>
+      <ExpandableText>{data ? data?.description_raw : ""}</ExpandableText>
     </>
   )
 }
